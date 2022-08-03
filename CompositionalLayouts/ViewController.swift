76b2401e42169e86fca3a6ae0df2269892f65239
Sergio Rodríguez Rama <ram.rod.ser+github@gmail.com>
2022-08-03 20:13:23 +0300

    :sparkles: basic compositional layout, looks like table view
    Removing storyboards.

--- CompositionalLayouts/ViewController.swift
+++ CompositionalLayouts/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Int>
+    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Int>
+    
+    private lazy var cellProvider: DataSource.CellProvider = { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell? in
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell_id", for: indexPath)
+        cell.backgroundColor = .label
+        return cell
+    }
+    
+    private lazy var dataSource: DataSource = {
+        return DataSource(collectionView: self.collectionView, cellProvider: self.cellProvider)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell_id")
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems([0, 1, 2, 3])
+        
+        self.dataSource.apply(snapshot, animatingDifferences: false)
     }
 
-
+    init() {
+        super.init(collectionViewLayout: {
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = .init(
+                top: 2,
+                leading: 16,
+                bottom: 2,
+                trailing: 16
+            )
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalWidth(2/3)
+            )
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: groupSize,
+                subitems: [item]
+            )
+            return UICollectionViewCompositionalLayout(section: .init(group: group))
+        }())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
-
